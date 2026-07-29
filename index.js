e050dcd9eb071e75323cdc47ec96663af75518b5
WillianGA02 <gabrielalmeidagraboski@gmail.com>
2026-07-29 16:53:02 -0300

    Segunda versão

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const p = document.createElement("p");
 
 const idade = 15;
 
-if (idade >= 18) {
+if (idade >= 18 && idade <= 50) {
   console.log("Voce é maior de idade tem " + idade + " anos");
   p.textContent = "Voce é maior de idade tem " + idade + " anos";
   app.appendChild(p);
-} else {
+} else if (idade < 18) {
     console.log("Voce é menor de idade tem " + idade + " anos");
     p.textContent = "Voce é menor de idade tem " + idade + " anos";
+    app.appendChild(p);
+} else {
+    console.log("Voce é idoso tem " + idade + " anos");
+    p.textContent = "Voce é idoso tem " + idade + " anos";
     app.appendChild(p);
 }
